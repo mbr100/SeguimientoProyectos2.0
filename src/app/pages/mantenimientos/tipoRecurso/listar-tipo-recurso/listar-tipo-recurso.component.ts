a661e863b69f98c9dcf76a8c25f055c7a87cb64d
Borregacion <mario.borrego100@gmail.com>
2024-01-19 00:48:08 +0100

    feat: Add TipoRecurso model and components for mantenimientos
    The commit adds the `TipoRecurso` model along with the necessary components for the `mantenimientos` feature. This includes the following components:
    - `agregar-tipo-recurso.component`
    - `listar-tipo-recurso.component`
    
    These components provide functionality for adding and listing tipo recursos.

---
+++ src/app/pages/mantenimientos/tipoRecurso/listar-tipo-recurso/listar-tipo-recurso.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-tipo-recurso',
+  standalone: true,
+  imports: [],
+  templateUrl: './listar-tipo-recurso.component.html',
+  styles: ``
+})
+export class ListarTipoRecursoComponent {
+
+}
