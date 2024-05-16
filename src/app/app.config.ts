6b187824c2878f9bd4bc31587fd9abe466585b9b
Borregacion <mario.borrego100@gmail.com>
2024-05-16 19:23:36 +0200

    Actualziación a angular fire ^17.1.0

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,7 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-import { provideClientHydration } from "@angular/platform-browser";
 import { AngularFireModule } from "@angular/fire/compat";
-import { initializeApp, provideFirebaseApp } from "@angular/fire/app";
 import { getAuth, provideAuth } from "@angular/fire/auth";
 import { getFirestore, provideFirestore } from "@angular/fire/firestore";
 import { environment } from "@environments/environment";
@@ -15,11 +12,11 @@ import { USE_EMULATOR as USE_AUTH_EMULATOR } from '@angular/fire/compat/auth';
 export const appConfig: ApplicationConfig = {
     providers: [
         provideRouter(routes),
-        provideClientHydration(),
-        importProvidersFrom(provideFirebaseApp(() => initializeApp(environment.firebase), environment.nombreApp)),
         importProvidersFrom(AngularFireModule.initializeApp(environment.firebase, environment.nombreApp)),
-        importProvidersFrom(provideAuth(() => getAuth())),
-        importProvidersFrom(provideFirestore(() => getFirestore())),
+        provideFirestore(()=>getFirestore()),
+        provideAuth(() => getAuth()),
+        provideFirestore(() => getFirestore()),
+        //provideFirebaseApp(() => initializeApp(environment.firebase), environment.nombreApp),
         //{ provide: USE_FIRESTORE_EMULATOR, useValue: !environment.production ? ['http://localhost', 8080] : undefined },
         //{ provide: USE_AUTH_EMULATOR, useValue: !environment.production ? ['http://localhost', 9099] : undefined },
     ]
